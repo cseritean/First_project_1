0cd9765f6b0cb2d19286da0609db4844a48a13d5
cseritean <cseritean@users.noreply.github.com>
2017-08-10 17:27:25 +0300

    mesaj

--- Big_Project.py
+++ Big_Project.py
@@ -3,3 +3,24 @@ nrx = randint(1, 50)
 
 print (nrx)
 
+dict = {"country": ["Brazil", "Russia", "India", "China", "South Africa"],
+        "capital": ["Brasilia", "Moscow", "New Dehli", "Beijing", "Pretoria"],
+        "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+        "population": [200.4, 143.5, 1252, 1357, 52.98] }
+
+import pandas as pd
+brics = pd.DataFrame(dict)
+print(brics)
+
+# Set the index for brics
+brics.index = ["BR", "RU", "IN", "CH", "SA"]
+
+# Print out brics with new index values
+print(brics)
+
+# Import the cars.csv data: cars
+cars = pd.read_csv('cars.csv')
+
+# Print out cars
+print(cars)
+print("mama")
